f52f093304d0508ef4ca67f9f5a02f6aa0da5b9f
BijalPrajapati786 <128888453+BijalPrajapati786@users.noreply.github.com>
2023-08-14 10:29:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import Stats from "./Stats";
+import PackingList from "./PackingList";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function hadleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirm = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirm) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={hadleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
